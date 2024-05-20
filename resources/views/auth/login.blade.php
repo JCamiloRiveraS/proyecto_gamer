e3ffaa9f60460a2b43d9a9fe31f1f51bb9546cb4
Camilo Rivera <102061094+JCamiloRiveraS@users.noreply.github.com>
2024-05-19 19:43:10 -0500

    si funciona, no lo toques bambaro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Registrar</title>
+    <title>Login</title>
     <link rel="stylesheet" href="{{ asset('css/style_register.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -14,9 +14,8 @@
 
 <body>
     <style>
-        body{
-            background-color: #9530e3;
-        }
+
+
         .form {
             display: flex;
             flex-direction: column;
@@ -30,6 +29,7 @@
             left: 36%;
             border: solid 2px black;
             margin-top: 10%;
+            z-index: 2;
         }
 
         .title {
@@ -158,6 +158,8 @@
         }
     </style>
 
+  
+
     <button><a href="{{ route('home') }}">home</a></button>
     <div class="container">
 
@@ -211,10 +213,10 @@
 
 
 
-
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
     </script>
--- resources/views/prueba/index.blade.php
+++ resources/views/prueba/index.blade.php
@@ -1,145 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <title>Gamify</title>
-
-  <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-  <!-- Additional CSS Files -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-       <!--fonts de google-->
-       <link
-       rel="stylesheet"
-       href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
-     />
-     <link
-       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-       rel="stylesheet"
-       integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-       crossorigin="anonymous"
-     />
-     <link rel="preconnect" href="https://fonts.googleapis.com" />
-     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-     <link
-       href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-       rel="stylesheet"
-     />
-    <link rel="stylesheet" href="{{ asset('css/style_prueba.css') }}">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>prueba</title>
 </head>
-
 <body>
-   <!--section barra lateral-->
-   <section class="barra_lateral">
-    <aside class="sidebar">
-      <div class="logo">
-        <img src="../../../public/images/images_home/g2.jpg" alt="logo" />
-        <h2>Usuario</h2>
-      </div>
-      <ul class="links">
-        <h4>Menú Principal</h4>
-        <li>
-          <span class="material-symbols-outlined">dashboard</span>
-          <a href="#">Panel</a>
-        </li>
-        <li>
-          <span class="material-symbols-outlined">Show_chart</span>
-          <a href="#">Contribuciónes</a>
-        </li>
-        <li>
-          <span class="material-symbols-outlined">flag</span>
-          <a href="#">Reportes</a>
-        </li>
-        <h4>Avanzado</h4>
-        <li>
-          <span class="material-symbols-outlined">person</span>
-          <a href="#">Usuario</a>
-        </li>
-        <li>
-          <span class="material-symbols-outlined">group</span>
-          <a href="#">Equipo</a>
-        </li>
-        <li>
-          <span class="material-symbols-outlined">ambient_screen</span>
-          <a href="#">Publicaciones</a>
-        </li>
-        <li>
-          <span class="material-symbols-outlined">pacemaker</span>
-          <a href="#">Conexiones</a>
-        </li>
-        <hr />
-        <h4>Cuenta</h4>
-        <li>
-          <span class="material-symbols-outlined">bar_chart</span>
-          <a href="#">Resumen</a>
-        </li>
-        <li>
-          <span class="material-symbols-outlined">mail</span>
-          <a href="#">Mensajes</a>
-        </li>
-        <li>
-          <span class="material-symbols-outlined">settings</span>
-          <a href="#">Configuración</a>
-        </li>
-        <li class="logout_link">
-          <span class="material-symbols-outlined">Logout</span>
-          <form action="{{ route('signOut') }}" method="POST">
-            @csrf
-            <a class="btn_log" ><button class="btn_log" type="submit">Cerrar Sesión</button></a>
-        </form>
-       
-        </li>
-      </ul>
-    </aside>
-  </section>
-  <!-- ***** Preloader Start ***** -->
-  <div id="js-preloader" class="js-preloader loaded">
-    <div class="preloader-inner">
-      <span class="dot"></span>
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-  </div>
- <!-- ***** Preloader End ***** -->
-<section class="contenido">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="most-popular">
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="heading-section">
-                <h4><em>Bienvenido a </em> Proyecto Gamer</h4>
-              </div>
-              <div class="row gameList">
-                
-              </div>
-              <div class="col-lg-12">
-                <div class="main-button hidden">
-                  <a href="javascript:void(0)">Load More Games</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-  <script src="{{ asset('js/dev.env.js') }}"></script>
-  <script src="{{ asset('js/script_prueba.js') }}"></script>
-</body>
+  <style>
+          #particles-js {
+            height: 100vh;
+            width: 100%;
+            position: fixed;
+            z-index: -1;
+        }
+  </style>
 
+  <div id="particles-js"></div>
+
+ <script src="/resources/js/particles.js"></script>
+<script src="/resources/js/particles.min.js"></script> 
+</body>
 </html>
